683d4c9e03d89557a54166c6980d1a5e50891a28
Yuval <yuvalgur16@gmail.com>
2026-05-09 16:29:45 +0300

    Moved analysis to the dashboard file. added visual charts and filtering ability

--- app/pipeline.py
+++ app/pipeline.py
@@ -6,11 +6,14 @@ from app.processing import cleaner
 from app.validation import generic_validator, job_market_validator
 
 
+
 def run_pipeline():
     file_path = get_raw_data_path('ds_salaries.csv')
     df = loader.load_data(file_path)
     df = cleaner.clean_data(df)
-    generic_validation_results = generic_validator.validate_dataframe(df)
-    job_market_validation_results = job_market_validator.validate_job_market(df)
+    validation_results = {
+        'generic_validation_results': generic_validator.validate_dataframe(df),
+        'job_market_validation_results' : job_market_validator.validate_job_market(df)
+    }
 
-    return df, generic_validation_results, job_market_validation_results
+    return df, validation_results
--- dashboard/streamlit_app.py
+++ dashboard/streamlit_app.py
@@ -1,17 +1,102 @@
 # Visualisation layer, will use streamlit to display the data in a clean way
 
 import streamlit as st
-from app.pipeline import run_pipeline
 
-df, validation_results, analysis_results = run_pipeline()
+from app.analysis import salary_analysis
+from app.pipeline import run_pipeline
+import pandas as pd
+
+df, validation_results = run_pipeline()
 
 st.title('Job Market Analyzer')
 st.markdown("Analyzing salary trends, roles, and outliers in the data science job market.")
-st.subheader("Dataset Preview")
-st.dataframe(df.head())
 
-st.subheader("Validation Results")
-st.write(validation_results)
+st.sidebar.header("Filters")
 
-st.subheader("Salary Analysis")
-st.write(analysis_results)
+experience_levels = sorted(df["experience_level"].unique())
+work_years = sorted(df["work_year"].unique())
+remote_ratios = sorted(df["remote_ratio"].unique())
+
+
+selected_experience = st.sidebar.selectbox(
+    "Experience Level",
+    options=["All"] + experience_levels
+)
+
+selected_year = st.sidebar.selectbox(
+    "Work Year",
+    options=["All"] + list(work_years)
+)
+
+selected_remote = st.sidebar.selectbox(
+    "Remote Ratio",
+    options=["All"] + list(remote_ratios)
+)
+
+filtered_df = df.copy()
+
+if selected_experience != "All":
+    filtered_df = filtered_df[
+        filtered_df["experience_level"] == selected_experience
+    ]
+
+if selected_year != "All":
+    filtered_df = filtered_df[
+        filtered_df["work_year"] == selected_year
+    ]
+
+if selected_remote != "All":
+    filtered_df = filtered_df[
+        filtered_df["remote_ratio"] == selected_remote
+    ]
+
+analysis_results = {
+        'salary_analysis' : salary_analysis.salary_analysis(filtered_df),
+        'outlier_analysis' : salary_analysis.salary_outliers_analysis(filtered_df)
+    }
+
+st.subheader("Average Salary by Experience Level")
+experience_df = pd.DataFrame(
+    analysis_results["salary_analysis"]['salary_by_experience'].items(),
+    columns=["Experience Level", "Average Salary"]
+)
+
+st.bar_chart(
+    experience_df,
+    x="Experience Level",
+    y="Average Salary"
+)
+
+
+top_jobs = (
+    analysis_results["salary_analysis"]["top_paying_jobs"]
+)
+top_jobs_df = pd.DataFrame(
+    top_jobs.items(),
+    columns=["Job Title", "Average Salary"]
+)
+
+st.subheader("Top Paying Jobs")
+
+st.bar_chart(
+    top_jobs_df,
+    x="Job Title",
+    y="Average Salary"
+)
+
+st.subheader("Salary by Remote Ratio")
+
+salary_by_remote = (
+    analysis_results["salary_analysis"]["salary_by_remote_ratio"]
+)
+remote_df = pd.DataFrame(
+    salary_by_remote.items(),
+    columns=["Remote Ratio", "Average Salary"]
+)
+st.subheader("Average Salary by Remote Ratio")
+
+st.bar_chart(
+    remote_df,
+    x="Remote Ratio",
+    y="Average Salary"
+)
